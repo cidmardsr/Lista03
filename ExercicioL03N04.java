33a76122cfbf8f0e1ccd979efd94a210138034d2
Cidmar da Silva Ribeiro <cidmardsr@gmail.com>
2018-04-27 11:11:38 -0300

    Correção exercicio 04

--- ExercicioL03N04.java
+++ ExercicioL03N04.java
@@ -4,35 +4,41 @@ public class ExercicioL03N04{
     public static void main(String[] args){
         int quantidadeCarros = Integer.parseInt(
                       JOptionPane.showInputDialog(null, 
-                      "Informe a quantidade de veiculos que deseja cadastrar"
+                "Informe a quantidade de veiculos que deseja cadastrar"
         ));
         int total = 0;
-
-        
+        int totalAno = 0;
+        double totalValor = 0;
+        int ano = 0;
+        double valor = 0;
         
         while(total < quantidadeCarros){
             String modelo = JOptionPane.showInputDialog(null, 
                 "Informe o modelo do veiculo");
-            double valor = Double.parseDouble(JOptionPane.showInputDialog(
+            valor = Double.parseDouble(JOptionPane.showInputDialog(
                 "Informe o valor do veiculo"
             ));
-            int ano = Integer.parseInt(JOptionPane.showInputDialog(
+            ano = Integer.parseInt(JOptionPane.showInputDialog(
                 "Informe o ano do veiculo"
             ));
             total = total + 1;
             
+            totalAno = totalAno + ano;
+            totalValor = totalValor + valor;
             
-            int mediaAno = ano * total / total;
-            double mediaValor = valor * total / total;
+            int mediaAno = totalAno / total;
+            double mediaValor = totalValor / total; 
 
-            JOptionPane.showMessageDialog(null, 
-                "A média do ano dos veiculos é: " 
-                + mediaAno +
-                "\nA média do valor dos carros é: " 
-                + mediaValor);
+            
+            
 
         }  
-        
-            
+            int mediaAno = totalAno / total;
+            double mediaValor = totalValor / total;
+            JOptionPane.showMessageDialog(null, 
+                "A média do ano dos veiculos é: " 
+            + mediaAno +
+                "\nA média do valor dos carros é: " 
+            + mediaValor);   
     }
 }
